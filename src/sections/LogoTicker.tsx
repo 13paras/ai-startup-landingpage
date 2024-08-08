8e1ca9ee05f1d8692edbacc85526d343de49ad62
Paras <sakarwalparas@gmail.com>
2024-08-08 11:57:56 +0530

    add: animation in logoticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
@@ -15,9 +16,24 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{ translateX: "-50%" }}
+              animate={{ translateX: "0" }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: "linear",
+              }}
+              className="flex flex-none -translate-x-1/2 gap-14 pr-14"
+            >
               {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestialLogo,
+                apexLogo,
+                quantumLogo,
                 acmeLogo,
                 pulseLogo,
                 echoLogo,
@@ -29,10 +45,10 @@ export const LogoTicker = () => {
                   src={logo}
                   key={logo.src}
                   alt=""
-                  className="h-6 w-auto"
+                  className="h-6 w-auto flex-none"
                 />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
