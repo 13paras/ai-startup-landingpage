365a0155e67ef26a894e4b6aaab7082494aabc67
Paras <sakarwalparas@gmail.com>
2024-08-08 10:03:45 +0530

    add: product showcase component

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import Image from "next/image";
+import productImage from "@/assets/product-image.png";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +32,43 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl md:text-6xl font-medium tracking-tighter">
+          Elevate your SEO efforts.
+        </h2>
+        <p className="mt-5 text-center text-lg tracking-tight text-white/70 md:text-xl max-w-2xl mx-auto">
+          From small startups to large enterprises, our AI-driven tool has
+          revolutionized the way businesses approach SEO
+        </p>
+        <div className="mt-10 flex flex-col lg:flex-row gap-3">
+          {tabs.map((tab) => (
+            <div
+              key={tab.title}
+              className="flex  items-center gap-2.5 rounded-xl border border-white/15 p-2.5 lg:flex-1"
+            >
+              <div className="inline-flex size-12 items-center justify-center rounded-lg border-white/15">
+                <DotLottiePlayer src={tab.icon} className="size-5" autoplay />
+              </div>
+              <div className="font-medium">{tab.title}</div>
+              {tab.isNew && (
+                <div className="rounded-full bg-[#8c44ff] px-2 py-0.5 text-xs font-semibold text-black">
+                  new
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+<div className="aspect-video bg-cover border border-white/20 rounded-lg" style={{
+  backgroundImage: `url(${productImage.src})`
+}}>
+
+</div>
+       
+        </div>
+      </div>
+    </section>
+  );
 };
